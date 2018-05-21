4c2dabd5342dfacf4d384e7f5607569ec37dd6f2
Alisina Bahadori <alisina.bm@gmail.com>
2018-05-22 00:49:13 +0430

    Fix tmux Home and End keys

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -26,6 +26,8 @@ bindkey '^[[1;5D' backward-word
 bindkey '^[[1;5C' forward-word
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
 
