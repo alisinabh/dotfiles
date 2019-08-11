a7c10ad60c4a257a35291246d8a765b58ef3f19c
Alisina Bahadori <alisina.bm@gmail.com>
2019-08-11 19:22:07 +0430

    Add rofi combi-modi

--- config/i3/scripts/rofi.sh
+++ config/i3/scripts/rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show run -font "monospace 18" -lines 5 -eh 2 -color-normal "#002f343f,#FFf3f4f5,#002f343f,#002f343f,#9575cd" -color-window "#DF2f343f,#FF2f343f,#8F2f343f" -width 100 -padding 250
+rofi -combi-modi window,drun,ssh -show combi -font "monospace 14" -lines 8 -eh 2 -color-normal "#002f343f,#FFf3f4f5,#002f343f,#002f343f,#9575cd" -color-window "#DF2f343f,#FF2f343f,#8F2f343f" -width 100 -padding 200
