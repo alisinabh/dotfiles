c3de1b2249ac6ab498671b375b01226280a753c6
Alisina Bahadori <alisina.bm@gmail.com>
2023-01-12 21:57:25 -0500

    Add solidity and improve cpu temp report

--- config/i3/scripts/cpu_temp.sh
+++ config/i3/scripts/cpu_temp.sh
@@ -2,7 +2,7 @@
 
 CPU_PACK_TEMP=$(sensors -j | jq '."coretemp-isa-0000"."Core 0"."temp2_input"')
 
-if [ "$CPU_PACK_TEMP" -gt "70" ];
+if [ "$CPU_PACK_TEMP" -gt "79" ];
 then
 	echo "<span color='red'> $CPU_PACK_TEMP°</span>";
 else
