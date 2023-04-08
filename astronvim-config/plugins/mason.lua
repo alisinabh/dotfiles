c5fa824b9668390a91b397c737ac9e43e37179e8
Alisina Bahadori <alisina.bm@gmail.com>
2023-04-08 14:36:23 -0400

    Switch back to mason elixirls

--- astronvim-config/plugins/mason.lua
+++ astronvim-config/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer", "solidity", "yamlls" },
+      ensure_installed = { "lua_ls", "elixirls", "rust_analyzer", "solidity", "yamlls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
