46f8509727797120e7b45cabc383d8caaa7123aa
Alisina Bahadori <alisina.bahadori@loblaw.ca>
2023-04-08 03:39:31 -0400

    Enhance astrovim config

--- astronvim-config/plugins/user.lua
+++ astronvim-config/plugins/user.lua
@@ -2,6 +2,23 @@ return {
   {
     "tpope/vim-fugitive",
     lazy = false
+  },
+  {
+    "mhanberg/elixir.nvim",
+    ft = { "elixir", "eex", "heex", "surface" },
+    config = function()
+      local elixir = require("elixir")
+
+      elixir.setup {
+        settings = elixir.settings {
+          dialyzerEnabled = false,
+          enableTestLenses = true,
+        }
+      }
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
