157643f030fb9c758ff1bcedd8ad477a98ab995e
Alisina Bahadori <alisina.bm@gmail.com>
2023-11-11 01:09:00 -0500

    Configure startify

--- config/nvim/lua/set.lua
+++ config/nvim/lua/set.lua
@@ -31,3 +31,31 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "100"
 
 vim.g.mapleader = " "
+
+-- Startify
+vim.g.startify_enable_special = 1
+vim.g.startify_files_number = 6
+vim.g.startify_change_to_dir = 0
+vim.g.startify_change_to_vcs_root = 0
+
+vim.g.startify_commands = {
+  { "Dotfiles",     'lua require("telescope.builtin").find_files({cwd = "~/.dotfiles", prompt_title="Find in Dotfiles"})' },
+  { "Lazy sync",    "Lazy sync" },
+  { "Lazy update",  "Lazy update" },
+  { "Lazy profile", "Lazy profile" },
+}
+
+function _G.webDevIcons(path)
+  local filename = vim.fn.fnamemodify(path, ":t")
+  local extension = vim.fn.fnamemodify(path, ":e")
+  return require("nvim-web-devicons").get_icon(filename, extension, { default = true })
+end
+
+vim.api.nvim_exec(
+  [[
+function! StartifyEntryFormat() abort
+  return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
+endfunction
+]],
+  false
+)
