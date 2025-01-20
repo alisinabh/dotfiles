792dbaf9a49ab383ee953860bd8737d29a556f8e
Alisina Bahadori <alisina.bm@gmail.com>
2025-01-20 01:35:41 -0500

    Improve lualine

--- config/nvim/lua/statusline.lua
+++ config/nvim/lua/statusline.lua
@@ -1,12 +1,13 @@
 require('lualine').setup {
   options = {
     theme = 'ayu_mirage',
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
+    component_separators = '',
+    section_separators = { left = '', right = '' },
+    always_show_tabline = false,
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', right_padding = 2 },
     },
     lualine_b = { 'filename', 'branch' },
     lualine_c = { 'diff' },
@@ -16,10 +17,11 @@ require('lualine').setup {
         'diagnostics',
         always_visible = false
       },
-      'filetype'
+      'filetype',
     },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      'progress',
+      { 'location', left_padding = 2 },
     },
   },
   inactive_sections = {
@@ -30,6 +32,24 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = { 'location' },
   },
-  tabline = {},
+  tabline = {
+    lualine_c = {
+      {
+        'tabs',
+        cond = function()
+          return #vim.fn.gettabinfo() > 1
+        end,
+        mode = 2,
+        use_mode_colors = true,
+        tabs_color = {
+          -- Same values as the general color option can be used here.
+          active = { fg = '#45afd6' },   -- Color for active tab.
+          inactive = { fg = '#246780' }, -- Color for inactive tab.
+
+        },
+        show_modified_status = true
+      }
+    },
+  },
   extensions = { 'neo-tree', 'quickfix' },
 }
